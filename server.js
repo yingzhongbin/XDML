c30708cde85eabf4091a88bdbd99d2c803b89415
yingzhongbin <36562860+yingzhongbin@users.noreply.github.com>
2018-04-03 09:00:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,21 +19,6 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log('方方说：得到 HTTP 路径\n' + path)
-  console.log('方方说：查询字符串为\n' + query)
-  console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
   console.log('HTTP 路径为\n' + path)
   if(path == '/style.css'){
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
@@ -55,14 +40,6 @@ var server = http.createServer(function(request, response){
     response.statusCode = 404
     response.end()
   }
-
-
-
-
-
-
-
-
 
 
   /******** 代码结束，下面不要看 ************/
